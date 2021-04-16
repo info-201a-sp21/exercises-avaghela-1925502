2699dedfa73dae510ad20e112fcaebef1cf76120
Arjun Vaghela <Arjun@MacBook-Pro-2-8.home>
2021-04-15 22:30:49 -0700

    chapter 8 exercise 2 complete

--- chapter-08-exercises/exercise-2/exercise.R
+++ chapter-08-exercises/exercise-2/exercise.R
@@ -3,40 +3,52 @@
 # Create a *list* of 10 random numbers. Use the `runif()` function to make a 
 # vector of random numbers, then use `as.list()` to convert that to a list
 
+rand_list <- as.list(runif(10, 1, 100))
 
 # Use `lapply()` to apply the `round()` function to each number, rounding it to 
 # the nearest 0.1 (one decimal place)
 
+lapply(rand_list, round, 1)
 
 # Create a variable 'sentence' that contains a sentence of text (something 
 # longish). Make the sentence lowercase; you can use a function to help.
 
+sentence <- tolower("My name is Arjun Vaghela and I like pie.")
 
 # Use the `strsplit()` function to split the sentence into a vector of letters.
 # Hint: split on `""` to split every character
 # Note: this will return a _list_ with 1 element (which is the vector of letters)
 
+letters <- strsplit(sentence, "")
 
 # Extract the vector of letters from the resulting list
 
+letters_extract <- letters[[1]]
 
 # Use the `unique()` function to get a vector of unique letters
 
+letters_unique <- unique(letters)
 
 # Define a function `count_occurrences` that takes in two parameters: a letter 
 # and a vector of letters. The function should return how many times that letter
 # occurs in the provided vector.
 # Hint: use a filter operation!
 
+count_occurrences <- function(letter, vector_letters) {
+  length(vector_letters[vector_letters == letter])
+}
 
 # Call your `count_occurrences()` function to see how many times the letter 'e'
 # is in your sentence.
 
+count_occurrences("e", letters_extract)
 
 # Use `sapply()` to apply your `count_occurrences()` function to each unique 
 # letter in the vector to determine their frequencies.
 # Convert the result into a list (using `as.list()`).
 
+frequencies <- as.list(sapply(letters_unique, count_occurrences, letters_extract))
 
 # Print the resulting list of frequencies
 
+print(frequencies)
