922908e411b8c5b7d410851a3fa228af87616bad
Arjun Vaghela <Arjun@MacBook-Pro-2.home>
2021-04-29 23:23:54 -0700

    chapter 11 exercise 2 complete

--- chapter-11-exercises/exercise-2/exercise.R
+++ chapter-11-exercises/exercise-2/exercise.R
@@ -8,41 +8,55 @@
 library(fueleconomy)
 
 # Install and load the "dplyr" library
-
+install.packages("dplyr")
+library("dplyr")
 
 # Select the different manufacturers (makes) of the cars in this data set. 
 # Save this vector in a variable
+make <- select(vehicles, make)
 
 
 # Use the `distinct()` function to determine how many different car manufacturers
 # are represented by the data set
+length(unique(make$make))
 
 
 # Filter the data set for vehicles manufactured in 1997
+cars_from_1997 <- filter(vehicles, year == 1997)
 
 
 # Arrange the 1997 cars by highway (`hwy`) gas milage
+cars_from_1997 <- arrange(cars_from_1997, hwy)
 
 
 # Mutate the 1997 cars data frame to add a column `average` that has the average
 # gas milage (between city and highway mpg) for each car
+cars_from_1997 <- mutate(cars_from_1997, average = ((hwy + cty) / 2))
 
 
 # Filter the whole vehicles data set for 2-Wheel Drive vehicles that get more
 # than 20 miles/gallon in the city. 
 # Save this new data frame in a variable.
+two_wheel_vehicles <- filter(vehicles, drive == "2-Wheel Drive", cty > 20)
 
 
 # Of the above vehicles, what is the vehicle ID of the vehicle with the worst 
 # hwy mpg?
 # Hint: filter for the worst vehicle, then select its ID.
-
+filtered <- filter(two_wheel_vehicles, hwy == min(hwy))
+worst <- select(filtered, id)
 
 # Write a function that takes a `year_choice` and a `make_choice` as parameters,
 # and returns the vehicle model that gets the most hwy miles/gallon of vehicles 
 # of that make in that year.
 # You'll need to filter more (and do some selecting)!
-
+make_year_filter <- function(make_choice, year_choice) {
+  filtered_1 <- filter(vehicles, make == make, year == year_choice)
+  filtered_1 <- filter(filtered_1, hwy == max(hwy))
+  selected_done <- select(filtered_1, model)
+  selected_done
+}
 
 # What was the most efficient Honda model of 1995?
-
+make_year_filter("Honda", 1995)
+#Civic HB VX
